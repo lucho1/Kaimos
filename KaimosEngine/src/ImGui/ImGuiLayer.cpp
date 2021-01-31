7b4b77d3d6779851fc79d287934aec2009f07b87
lucho1 <luchosuaya@hotmail.com>
2021-01-31 12:49:40 +0100

    Fixed problem on loading fonts on ImGui Font Load & Added Fonts folder to editor

--- KaimosEngine/src/ImGui/ImGuiLayer.cpp
+++ KaimosEngine/src/ImGui/ImGuiLayer.cpp
@@ -131,18 +131,12 @@ namespace Kaimos {
 		// Careful in the next lines of codes because the path is not relative, is absolute, so it will work only in one PC, otherwise we will have to change it
 		
 		// --- Fonts Load ---
-		// For desktop-PC (Lucho): D:/GitHub/Kaimos/KaimosEngine/res/fonts/
-		// For portable-PC (Lucho): C:/Users/Lucho/Documents/GitHub/Kaimos/KaimosEngine/res/fonts/
-		// TODO: Change this for god sake, add a filesystem
-		std::string init_path = "D:";		
-		if (false)										// By now just change this to true when on PortablePC
-			init_path = "C:/Users/Lucho/Documents";
+		std::string font_path = "../KaimosEngine/res/fonts/Ruda-SemiBold.ttf";
 
-		init_path += "/GitHub/Kaimos/KaimosEngine/res/fonts/Ruda-Bold.ttf";
-		io.Fonts->AddFontFromFileTTF(init_path.c_str(), 12);
-		io.Fonts->AddFontFromFileTTF(init_path.c_str(), 10);
-		io.Fonts->AddFontFromFileTTF(init_path.c_str(), 14);
-		io.Fonts->AddFontFromFileTTF(init_path.c_str(), 18);
+		io.Fonts->AddFontFromFileTTF(font_path.c_str(), 12);
+		io.Fonts->AddFontFromFileTTF(font_path.c_str(), 10);
+		io.Fonts->AddFontFromFileTTF(font_path.c_str(), 14);
+		io.Fonts->AddFontFromFileTTF(font_path.c_str(), 18);
 		// --- ---
 
 		// --- UI Style Settings ---
