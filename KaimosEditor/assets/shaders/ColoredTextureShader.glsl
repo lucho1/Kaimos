f5df988d17e2ad50bcda523e9a1e911d468804f7
lucho1 <luchosuaya@hotmail.com>
2021-02-13 10:50:59 +0100

    Update ColoredTextureShader.glsl
    Just to solve a problem with ZFighting

--- KaimosEditor/assets/shaders/ColoredTextureShader.glsl
+++ KaimosEditor/assets/shaders/ColoredTextureShader.glsl
@@ -12,7 +12,7 @@ uniform mat4 u_ViewProjection;
 
 out vec2 v_TexCoord;
 out vec4 v_Color;
-out float v_TexIndex;
+out flat float v_TexIndex;
 out float v_TilingFactor;
 
 void main()
@@ -35,7 +35,7 @@ layout(location = 0) out vec4 color;
 
 in vec2 v_TexCoord;
 in vec4 v_Color;
-in float v_TexIndex;
+in flat float v_TexIndex;
 in float v_TilingFactor;
 
 uniform sampler2D u_Textures[32];
