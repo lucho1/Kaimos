700cf91f5224120381921b286fad16ff0e5a89b7
lucho1 <luchosuaya@hotmail.com>
2021-02-24 18:47:02 +0100

    Update on code syntax for Random Generator

--- KaimosEngine/src/Core/Utils/RandomGenerator.h
+++ KaimosEngine/src/Core/Utils/RandomGenerator.h
@@ -6,22 +6,26 @@
 
 namespace Kaimos::Random
 {
-	static std::random_device s_Seed;
-	static std::default_random_engine s_Engine = std::default_random_engine(s_Seed());
+	typedef std::uniform_int_distribution<int> IntDist;
+	typedef std::uniform_real_distribution<double> DoubleDist;
 
-	static std::uniform_int_distribution<int> s_IntDistribution;
-	static std::uniform_real_distribution<double> s_DoubleDistribution;
+	static struct RNGData
+	{
+		std::random_device s_Seed;
+		std::default_random_engine s_Engine = std::default_random_engine(s_Seed());
+	}RNGData;
 
 	// Returns a random int in range (including both)
 	// If both values are 0, it will return an un-ranged random int
 	static int GetRandomInt(int range_min = 0, int range_max = 0)
 	{
 		if (range_min == 0 && range_max == 0)
-			return s_IntDistribution(s_Engine);
+			return IntDist()(RNGData.s_Engine);
 
 		KS_ENGINE_ASSERT(range_min < range_max, "RangeMin has to be lower than RangeMax");
-		std::uniform_int_distribution<int> dist(range_min, range_max);
-		return dist(s_Engine);
+
+		IntDist dist(range_min, range_max);
+		return dist(RNGData.s_Engine);
 	}
 
 	// Returns a random vector of ints in range (including both)
@@ -29,21 +33,15 @@ namespace Kaimos::Random
 	static std::vector<int> GetRandomIntVector(int vec_size, int range_min = 0, int range_max = 0)
 	{
 		std::vector<int> ret;
-		std::uniform_int_distribution<int>* dist;
+		IntDist* dist;
 
 		if (range_min == 0 && range_max == 0)
-		{
-			std::uniform_int_distribution<int> d;
-			dist = &d;
-		}
+			dist = &IntDist();
 		else
-		{
-			std::uniform_int_distribution<int> d(range_min, range_max);
-			dist = &d;
-		}
+			dist = &IntDist(range_min, range_max);
 
 		for (int i = 0; i < vec_size; ++i)
-			ret.push_back((*dist)(s_Engine));
+			ret.push_back((*dist)(RNGData.s_Engine));
 
 		return ret;
 	}
@@ -54,11 +52,12 @@ namespace Kaimos::Random
 	static float GetRandomFloat(int range_min = 0, int range_max = 0)
 	{
 		if (range_min == 0 && range_max == 0)
-			return (float)s_DoubleDistribution(s_Engine);
+			return (float)DoubleDist()(RNGData.s_Engine);
 
 		KS_ENGINE_ASSERT(range_min < range_max, "RangeMin has to be lower than RangeMax");
-		std::uniform_real_distribution<double> dist((double)range_min, (double)range_max);
-		return (float)dist(s_Engine);
+
+		DoubleDist dist((double)range_min, (double)range_max);
+		return (float)dist(RNGData.s_Engine);
 	}
 
 	// Returns a random vector of floats in range (including both)
@@ -67,21 +66,15 @@ namespace Kaimos::Random
 	static std::vector<float> GetRandomFloatVector(int vec_size, int range_min = 0, int range_max = 0)
 	{
 		std::vector<float> ret;
-		std::uniform_real_distribution<double>* dist;
+		DoubleDist* dist;
 
 		if (range_min == 0 && range_max == 0)
-		{
-			std::uniform_real_distribution<double> d;
-			dist = &d;
-		}
+			dist = &DoubleDist();
 		else
-		{
-			std::uniform_real_distribution<double> d((double)range_min, (double)range_max);
-			dist = &d;
-		}
+			dist = &DoubleDist((double)range_min, (double)range_max);
 
 		for (int i = 0; i < vec_size; ++i)
-			ret.push_back((float)(*dist)(s_Engine));
+			ret.push_back((float)(*dist)(RNGData.s_Engine));
 
 		return ret;
 	}
