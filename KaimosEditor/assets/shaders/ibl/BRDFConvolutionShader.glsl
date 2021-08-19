3f955eab9b17949ddcf0f865959515cd47a07aaf
Lucho Suaya <luchosuaya@hotmail.com>
2021-08-20 00:24:03 +0200

    Moved IBL Shaders to a special folder

--- KaimosEditor/assets/shaders/ibl/BRDFConvolutionShader.glsl
+++ KaimosEditor/assets/shaders/ibl/BRDFConvolutionShader.glsl
@@ -29,38 +29,7 @@ vec2 Hammersley(uint i, uint N);
 vec3 ImportanceSamplingGGX(vec2 xi, vec3 N, float roughness);
 float GeometrySchlickGGX(float NdotV, float roughness);
 float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness);
-
-vec2 IntegrateBRDF(float NdotV, float roughness)
-{
-	vec3 V = vec3(sqrt(1.0 - NdotV*NdotV), 0.0, NdotV);
-	vec3 N = vec3(0.0, 0.0, 1.0);
-	
-	float A = 0.0, B = 0.0;
-	for(uint i = 0u; i < SAMPLE_COUNT; ++i)
-	{
-		vec2 xi = Hammersley(i, SAMPLE_COUNT);
-		vec3 H = ImportanceSamplingGGX(xi, N, roughness);
-		vec3 L = normalize(2.0 * dot(V, H) * H - V);
-
-		float NdotL = max(L.z, 0.0);
-		float NdotH = max(H.z, 0.0);
-		float VdotH = max(dot(V, H), 0.0);
-
-		if(NdotL > 0.0)
-		{
-			float G = GeometrySmith(N, V, L, roughness);
-			float G_vis = (G*VdotH) / (NdotH*NdotV);
-			float Fc = pow(1.0 - VdotH, 5.0);
-
-			A += (1.0 - Fc)*G_vis;
-			B += Fc*G_vis;
-		}
-	}
-
-	A /= float(SAMPLE_COUNT);
-	B /= float(SAMPLE_COUNT);
-	return vec2(A, B);
-}
+vec2 IntegrateBRDF(float NdotV, float roughness);
 
 
 // - Main -
@@ -124,4 +93,36 @@ float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
 	float ggx2 = GeometrySchlickGGX(NdotV, roughness);
 	float ggx1 = GeometrySchlickGGX(NdotL, roughness);
 	return ggx1 * ggx2;
+}
+
+vec2 IntegrateBRDF(float NdotV, float roughness)
+{
+	vec3 V = vec3(sqrt(1.0 - NdotV*NdotV), 0.0, NdotV);
+	vec3 N = vec3(0.0, 0.0, 1.0);
+	
+	float A = 0.0, B = 0.0;
+	for(uint i = 0u; i < SAMPLE_COUNT; ++i)
+	{
+		vec2 xi = Hammersley(i, SAMPLE_COUNT);
+		vec3 H = ImportanceSamplingGGX(xi, N, roughness);
+		vec3 L = normalize(2.0 * dot(V, H) * H - V);
+
+		float NdotL = max(L.z, 0.0);
+		float NdotH = max(H.z, 0.0);
+		float VdotH = max(dot(V, H), 0.0);
+
+		if(NdotL > 0.0)
+		{
+			float G = GeometrySmith(N, V, L, roughness);
+			float G_vis = (G*VdotH) / (NdotH*NdotV);
+			float Fc = pow(1.0 - VdotH, 5.0);
+
+			A += (1.0 - Fc)*G_vis;
+			B += Fc*G_vis;
+		}
+	}
+
+	A /= float(SAMPLE_COUNT);
+	B /= float(SAMPLE_COUNT);
+	return vec2(A, B);
 }
